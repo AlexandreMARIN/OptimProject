54418d28ae4fec1b8514e26441ceaa6cc9ef17de
Alexandre MARIN <alexandre.marin1@laposte.net>
2019-12-05 23:09:16 +0100

    I have added some comments and improved the files odefunc.m and test_ode.m.

--- odefunc.m
+++ odefunc.m
@@ -1,5 +1,10 @@
 function dydt = odefunc(t, y, alpha, M_i, v_e, theta)
-%y := R, V, M
+%this function must be used with ode45()
+%Input:
+%t, y : for ode45
+%here, y represents [R, V, M]
+%other parameters : given by the problem
+
 global R_t
 
 mu = 3.986e14;
@@ -7,30 +12,34 @@ c_x = 0.1;
 rho0 = 1.225;
 H = 7e3;
 
-rho = rho0 * exp( (R_t - norm(y(1:2)))/H );
-Tmag = alpha*M_i;
-
+%distance between the rocket and the center of the Earth
 R = norm(y(1:2));
+%velocity of the rocket
 V = norm(y(3:4));
+
+rho = rho0 * exp( (R_t - R)/H );
+Tmag = alpha*M_i;
 
 dydt = zeros(5, 1);
 
-dydt(1) = y(3);
-dydt(2) = y(4);
+%\dot{R} = V
+dydt(1:2) = y(3:4);
 
+%forces
+W = -(mu*y(5)/R^3)*y(1:2);
+D = -c_x*rho*V*y(3:4);
 
-W = -(mu*y(5)/norm(y(1:2))^3)*y(1:2);
-D = -c_x*rho*norm(y(3:4))*y(3:4);
-
-e_r = y(1:2)/norm(y(1:2));
+e_r = y(1:2)/R;
 e_h = [-e_r(2) ; e_r(1)];
 gamma = asin(y(1:2)'*y(3:4)/(R*V));
 angle = theta + gamma;
 u = cos(angle)*e_h + sin(angle)*e_r;
 T = Tmag*u;
 
+%Fundamental principle of the dynamics
 dydt(3:4) = (T + W + D)/y(5);
 
+%derivative of the mass
 dydt(5) = -Tmag/v_e;
 
 end
--- tests/test_ode.m
+++ tests/test_ode.m
@@ -1,3 +1,4 @@
+addpath("../")
 global R_t
 
 R_t = 6378137;
@@ -13,11 +14,27 @@ m_e = 145349;
 tf = m_e*v_e/(alpha*M0);
 t0 = 0;
 
+
+%for the Cauchy problem
 y0 = [R0 ; V0 ; M0];
 tspan = linspace(t0, tf, 1000);
 
+%we solve the ODE
 [t, y] = ode45(@(t, y) odefunc(t, y, alpha, M0, v_e, theta0), tspan, y0);
+
 close all
-viscircles([0 0], R_t)
+%we draw the Earth
+angle = linspace(-pi/200, pi/200, 1000);
+xEarth = R_t*cos(angle);
+yEarth = R_t*sin(angle);
+plot(xEarth, yEarth)
 hold on
+axis equal
+
+%we draw the path
 plot(y(:,1), y(:,2))
+
+title("Example: the first part of the path")
+xlabel("x")
+ylabel("y")
+legend("Earth", "rocket")
